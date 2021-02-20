457791baea9bcc02528caca8f3eb727ba0da54b7
Haribalan <haribalan.r@photoninfotech.net>
2021-02-20 11:57:36 +0530

    Swagger def added

--- backend/src/docs/swaggerDef.js
+++ backend/src/docs/swaggerDef.js
@@ -8,7 +8,7 @@ const swaggerDef = {
     version,
     license: {
       name: 'MIT',
-      url: 'https://github.com/hagopj13/node-express-boilerplate/blob/master/LICENSE',
+      url: 'https://github.com/harievenad05/payment_react_native/blob/dev/backend/LICENSE',
     },
   },
   servers: [
